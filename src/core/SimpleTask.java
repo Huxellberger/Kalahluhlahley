9bfc8d3b9196c5c215d0c0f9daef63b2f9847578
Huxellberger <jake.huxell@student.manchester.ac.uk>
2017-12-05 15:16:44 +0000

    removed uneeded information from SimpleTask

--- src/core/SimpleTask.java
+++ src/core/SimpleTask.java
@@ -4,15 +4,11 @@ import java.util.concurrent.Callable;
 
 public class SimpleTask implements Callable<ExpansionTaskResult>
 {
-    private Board startingBoard;
-    private Board simulationBoard;
     private int startingMove;
     private int timeout;
 
     public SimpleTask(Board inBoard, int inMove, float inTimeout) throws CloneNotSupportedException
     {
-	startingBoard = inBoard.clone();
-	simulationBoard = null;
 	startingMove = inMove;
 	timeout = (int)inTimeout;
     }
